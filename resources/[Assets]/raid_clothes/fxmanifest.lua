70ab69958d82ee53df92ecd866637d92c54a9283
Aspecttt2566 <118882930+Aspecttt2566@users.noreply.github.com>
2023-09-21 01:15:45 +0100

    a

---
+++ resources/[Assets]/raid_clothes/fxmanifest.lua
@@ -0,0 +1,46 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+--[[ dependencies {
+  "np-lib"
+} ]]--
+
+ui_page 'client/html/index.html'
+
+files {
+  'client/html/*.html',
+  'client/html/*.js',
+  'client/html/*.css',
+  'client/html/webfonts/*.eot',
+  'client/html/webfonts/*.svg',
+  'client/html/webfonts/*.ttf',
+  'client/html/webfonts/*.woff',
+  'client/html/webfonts/*.woff2',
+  'client/html/css/*',
+}
+
+client_scripts {
+  '@np-errorlog/client/cl_errorlog.lua',
+  '@np-lib/client/cl_rpc.lua',
+  '@np-lib/client/cl_ui.lua',
+  'client/cl_tattooshop.lua',
+  'client/cl_*.lua',
+}
+
+shared_script {
+  '@np-lib/shared/sh_util.lua',
+  'shared/sh_*.*',
+}
+
+server_scripts {
+  '@np-lib/server/sv_rpc.lua',
+  'server/sv_*.lua',
+}
+
+server_script "@oxmysql/lib/MySQL.lua"
+
+
+export "CreateHashList"
+export "GetTatCategs"
+export "GetCustomSkins"
+export "isNearClothing"
